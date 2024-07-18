f1eb16818c2969b731acec6bc686c7801db90ee1
Sesajeng <sesaajeng780@gmail.com>
2024-07-18 11:36:46 +0700

    ubah

---
+++ app/Http/Controllers/LaporInsidenController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LaporInsidenSiberController extends Controller
+{
+    public function index(){
+        return view('lapor_insiden_siber');
+        }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfilController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\PublikasiController;
+use App\Http\Controllers\KontakKamiController;
+use App\Http\Controllers\LaporInsidenSiberController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('welcome', [HomeController::class, 'index'])->name('welcome');
+Route::get('profil', [ProfilController::class, 'index'])->name('profil');
+Route::get('event',[EventController::class, 'index'])->name('event');
+Route::get('publikasi',[PublikasiController::class, 'index'])->name('publikasi');
+Route::get('kontak_kami',[KontakKamiController::class, 'index'])->name('kontak_kami');
+Route::get('lapor_insiden_siber', [LaporInsidenSiberController::class, 'index'])->name('lapor_insiden_siber');
